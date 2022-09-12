27a8dbf4b070c92e511b3cada5e53cd11431c53a
jr-cast <castanos@pm.me>
2022-09-12 18:12:28 -0500

    Add basic models

--- db/migrate/20220912171034_add_details_to_bills.rb
+++ db/migrate/20220912171034_add_details_to_bills.rb
@@ -1,6 +1,6 @@
 class AddDetailsToBills < ActiveRecord::Migration[7.0]
   def change
     add_column :bills, :name, :string
-    add_column :bills, :amount, :decimal
+    add_column :bills, :amount, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_12_171941) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.decimal "amount"
+    t.integer "amount"
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_bills_on_user_id"
   end
